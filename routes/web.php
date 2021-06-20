b79aa48959e83ace6dbbb79cefa2963a9e4a8232
Bartosz Sieprawski <b.sieprawski@op.pl>
2021-06-21 00:48:33 +0200

    Use the new syntax for Controllers

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\IndexController;
 
-Route::get('/', "IndexController@index")->name('index');
+Route::get('/', [IndexController::class, 'index'])->name('index');
