59a1d727d2c0c3d3af7baf2730a9b003534a7d11
Bartosz Sieprawski <b.sieprawski@op.pl>
2021-06-25 17:55:49 +0200

    Temporarily add TestRoutes for documentation purposes
    * removes current comments, remains as TODO

--- routes/web/main.php
+++ routes/web/main.php
@@ -13,56 +13,25 @@ use Illuminate\Support\Facades\Route;
  * The main purpose is to keep the Routes separated.
  * To make it a bit more clear, check the Structure below how to keep the Routes separated.
  * 
- * Let's assume we have a MyProfile page, which has different Controllers
- * - Dashboard
- * - Settings
- * - Stats
- * 
- * The structure of these Routes should be as following:
- * 
- * |-  routes
- * |   |-  web/
- * |   |   |- profile
- * |   |   |   |- settings.php
- * |   |   |   |- stats.php
- * |   |   |   |- dashboard.php
- * |   |   |- index.php (basic Index)
- * |   |   |- main.php (main file - route loader)
- * 
- * and below are the definitions of this structure:
- * 
- * $profileRoutePrefix = __DIR__ . '/profile';
- * Route::prefix('profile')->group($profileRoutePrefix . 'profile/dashboard.php');
- * Route::prefix('profile/stats')->group($profileRoutePrefix . 'profile/stats.php');
- * Route::prefix('profile/settings')->group($profileRoutePrefix . 'profile/settings.php');
- * 
- * also the following is valid for more readable group:
- * 
- * Route::prefix("profile")->group(function () {
- *     $profileRoutePrefix = __DIR__ . '/profile';
- * 
- *     Route::prefix('/')->group($profileRoutePrefix . '/dashboard.php');
- *     Route::prefix('/stats')->group($profileRoutePrefix . '/stats.php');
- *     Route::prefix('/settings')->group($profileRoutePrefix . '/settings.php');
- * });
- * 
- * NOTE: There is no need to prefix the routes inside the target files, this
- * is the only place the prefix is required
- * 
- * and URLs for those routes:
- * 
- * /profile
- * /profile/stats
- * /profile/settings
- * 
- * Middleware can be assigned either in the file itself - if not all routes need them -
- * or in this file, but it would be a good practice to keep middlewares in one file.
- * Route::prefix()->middleware()->group()
- * 
- * Moving this to RouteServiceProvider would be nice, but I haven't found a way ^^.
+ * TODO!!!
  */
 
-/**
- * Index Routes
- */
+// Index Routes
 Route::prefix('/')->group(__DIR__ . '/index.php');
+
+// Admin Routes Group
+$adminPathPrefix = __DIR__ . $adminPrefix = "/admin";
+Route::prefix($adminPrefix)
+    ->group(function () use ($adminPathPrefix, $adminPrefix) {
+        Route::prefix('/')->group($adminPathPrefix . '/admin.php');
+        Route::prefix('/statistics')->group($adminPathPrefix . '/statistics.php');
+
+        // Forum Settings Routes Group
+        $adminForumPathPrefix = __DIR__ . $adminForumPrefix = $adminPrefix . "/forum-settings";
+        Route::prefix($adminForumPrefix)->group(function () use ($adminForumPathPrefix, $adminForumPrefix) {
+            Route::prefix('/')->group($adminForumPathPrefix . '/forum-settings.php');
+            Route::prefix('/threads')->group($adminForumPathPrefix . '/threads.php');
+            Route::prefix('/posts')->group($adminForumPathPrefix . '/posts.php');
+            Route::prefix('/users')->group($adminForumPathPrefix . '/users.php');
+        });
+    });
