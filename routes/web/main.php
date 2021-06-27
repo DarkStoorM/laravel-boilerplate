d28fad0f07e002998550d91d1cf7acff77fbbc8b
Bartosz Sieprawski <b.sieprawski@op.pl>
2021-06-27 15:40:16 +0200

    Remove test routes
    This has been moved to README section explaining Route Separation

--- routes/web/main.php
+++ routes/web/main.php
@@ -11,27 +11,10 @@ use Illuminate\Support\Facades\Route;
  * in this file, as this file serves as a Route File Loader.
  * 
  * The main purpose is to keep the Routes separated.
- * To make it a bit more clear, check the Structure below how to keep the Routes separated.
  * 
- * TODO!!!
+ * Everyone has his own way of organizing Routes, so this file will be left undocumented
+ * 
  */
 
 // Index Routes
 Route::prefix('/')->group(__DIR__ . '/index.php');
-
-// Admin Routes Group
-$adminPathPrefix = __DIR__ . $adminPrefix = "/admin";
-Route::prefix($adminPrefix)
-    ->group(function () use ($adminPathPrefix, $adminPrefix) {
-        Route::prefix('/')->group($adminPathPrefix . '/admin.php');
-        Route::prefix('/statistics')->group($adminPathPrefix . '/statistics.php');
-
-        // Forum Settings Routes Group
-        $adminForumPathPrefix = __DIR__ . $adminForumPrefix = $adminPrefix . "/forum-settings";
-        Route::prefix($adminForumPrefix)->group(function () use ($adminForumPathPrefix, $adminForumPrefix) {
-            Route::prefix('/')->group($adminForumPathPrefix . '/forum-settings.php');
-            Route::prefix('/threads')->group($adminForumPathPrefix . '/threads.php');
-            Route::prefix('/posts')->group($adminForumPathPrefix . '/posts.php');
-            Route::prefix('/users')->group($adminForumPathPrefix . '/users.php');
-        });
-    });
