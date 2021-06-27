83d52adbd7ae33f9a716fe0a9d7d4d28250508af
Bartosz Sieprawski <b.sieprawski@op.pl>
2021-06-27 18:37:33 +0200

    Register a basic Helper
    * changes the autoload path

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!function_exists('testHelper')) {
+    /**
+     * This is just a test function for Helpers Autoload
+     * 
+     * Remove this
+     */
+    function testHelper(): void
+    {
+        // do nothing
+    }
+}
