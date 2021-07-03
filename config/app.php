ef7e24cf7a4ae4e5c6ce75830326b7230842efde
Bartosz Sieprawski <b.sieprawski@op.pl>
2021-07-03 19:18:49 +0200

    Implement Debugbar --dev, move /Whoops to Dev
    Debugbar has the default configuration

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -228,6 +229,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
-
 ];
