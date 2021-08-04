e5fa47ff01f3e73ab1f80f16d4ff1e5acaadd27e
Bartosz Sieprawski <b.sieprawski@op.pl>
2021-08-04 20:54:12 +0200

    Register custom libraries

--- config/app.php
+++ config/app.php
@@ -228,6 +228,9 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        "RouteNames" => App\Libs\Utils\RouteNames::class,
+        "Constants" => App\Libs\Constants::class,
+
     ],
 
 ];
