d3ccb92466d871c6a254187fe240ec2b5e487017
Bartosz Sieprawski <b.sieprawski@op.pl>
2021-08-04 20:06:30 +0200

    Add form requests

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Libs\Constants;
+use App\Rules\Throttle;
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            "email" => [
+                "required",
+                "email",
+                new Throttle(
+                    "throttle-login",
+                    Constants::LOGIN_MAXIMUM_ATTEMPTS,
+                    Constants::LOGIN_THROTTLE_TIMEOUT,
+                    trans("login.validation.throttled")
+                )
+            ],
+            "password" => "required"
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            "email.required" => trans("login.validation.email-required"),
+            "email.email" => trans("login.validation.email-invalid"),
+            "password.required" => trans("login.validation.password-required"),
+        ];
+    }
+}
---
+++ app/Http/Requests/PasswordResetRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Libs\Constants;
+use App\Rules\Throttle;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+
+class PasswordResetRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            "email" => [
+                "required",
+                "email",
+                new Throttle(
+                    "throttle-password-reset",
+                    Constants::PASSWORD_RESET_MAX_REQUEST_ATTEMPTS,
+                    Constants::PASSWORD_RESET_THROTTLE_TIMEOUT,
+                    trans("password_reset.validation.throttled")
+                )
+            ]
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            "email.required" => trans("password_reset.validation.email-required"),
+            "email.email" => trans("password_reset.validation.email-invalid"),
+        ];
+    }
+
+    public function prepareForValidation(): Request
+    {
+        return $this->merge([
+            'email' => strtolower($this->email),
+        ]);
+    }
+}
