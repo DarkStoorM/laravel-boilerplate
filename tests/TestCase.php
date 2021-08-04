a8f7e55732e2cf4b4c4a13326eb1647153a104e4
Bartosz Sieprawski <b.sieprawski@op.pl>
2021-08-04 19:58:00 +0200

    Update Test Cases

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,27 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, DatabaseMigrations, WithFaker;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+
+        // This email will be used for new users that have to be created separately
+        // - entering a non-existing email
+        $this->fakeEmail = $this->faker->safeEmail();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
 }
