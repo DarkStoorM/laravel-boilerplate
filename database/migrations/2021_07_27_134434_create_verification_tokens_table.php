707d3a1d9e23dd72dff94c09fb428f293ad7b37b
Bartosz Sieprawski <b.sieprawski@op.pl>
2021-08-04 19:59:14 +0200

    Update migrations

--- database/migrations/2021_07_27_134434_create_verification_tokens_table.php
+++ database/migrations/2021_07_27_134434_create_verification_tokens_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateVerificationTokensTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
+        Schema::create('verification_tokens', function (Blueprint $table) {
             $table->id();
             $table->string('email');
             $table->string('token', 50);
@@ -29,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('verification_tokens');
     }
 }
