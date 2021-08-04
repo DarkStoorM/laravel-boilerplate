0b31e3f31ed8fd58e86da936003eaf6eccf2206a
Bartosz Sieprawski <b.sieprawski@op.pl>
2021-08-04 20:00:22 +0200

    Add throttle localization key

---
+++ resources/lang/en/generic.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * This file is reserved for generic localization not strictly related to a bigger group
+ */
+return [
+    "throttled" => "Too many attempts, please try again after some time."
+];
